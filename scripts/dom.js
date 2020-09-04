dfd91a06b9b79e84ebc18ca8bacdf7c0615017fb
teekaytech <taofeek.olalere@feghas.com>
2020-09-04 13:55:37 +0100

    Fix import/export issues

--- scripts/dom.js
+++ scripts/dom.js
@@ -1,5 +1,9 @@
-const startButton = document.getElementById('start');
-const gameTable = document.querySelector('.board');
+const Elements = (() => {
+  const startButton = document.getElementById('start');
+  const gameTable = document.querySelector('.board');
+  const messageContainer = document.querySelector('.messages');
 
-console.log(startButton);
-console.log(gameTable);
+  return { startButton, gameTable, messageContainer };
+})();
+
+export default { Elements };
--- scripts/game.js
+++ scripts/game.js
@@ -1,10 +1,13 @@
-import Board from './board';
-import Player from './player';
+/* eslint-disable import/extensions */
 
+import Elements from './dom.js';
+import Board from './board.js';
+import Player from './player.js';
 
 const firstPlayer = Player();
 const gameBoard = Board();
 
+console.log(Elements);
 console.log(firstPlayer.getName());
 console.log(gameBoard.getMoves());
 // secondPlayer = Player();
